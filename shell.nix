9ef2fc5d27007f8892aeec459a8850b29784735d
xTrayambak <xtrayambak@disroot.org>
2024-09-01 13:39:24 +0530

    (add) nix: add nix shell

---
+++ shell.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> { };
+
+mkShell {
+  nativeBuildInputs = [
+  	gtk4.dev
+	libadwaita.dev
+	pkg-config
+  	openssl.dev
+  ];
+
+  LD_LIBRARY_PATH = lib.makeLibraryPath [
+  	gtk4.dev
+	libadwaita.dev
+	pkg-config
+  	openssl.dev
+  ];
+}
