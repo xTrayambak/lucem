c61d8cc65cd6c857161e4c185f1692e9c745ee2d
reflexran <151042611+reflexran@users.noreply.github.com>
2024-09-08 17:39:16 +0200

    Remove outdated feature

--- src/commands/init.nim
+++ src/commands/init.nim
@@ -1,14 +1,12 @@
-## This file implements `lucem init`, `lucem install-sober` and `lucem fetch-apk`
+## This file implements `lucem init` and `lucem install-sober`
 ## Copyright (C) 2024 Trayambak Rai
 
-import std/[logging, browsers, rdstdin, strutils]
-import ../[flatpak, argparser, config, common]
+import std/[logging, strutils]
+import ../[flatpak, argparser, common]
 
 const
   SOBER_FLATPAK_URL* {.strdefine: "SoberFlatpakUrl".} =
     "https://sober.vinegarhq.org/sober.flatpakref"
-  APKMIRROR_URL* {.strdefine: "ApkMirrorUrl".} =
-    "https://www.apkmirror.com/apk/roblox-corporation/roblox/roblox-$1-release/roblox-$1-android-apk-download"
 
 proc initializeSober*(input: Input) {.inline.} =
   info "lucem: initializing sober"
@@ -18,36 +16,6 @@ proc initializeSober*(input: Input) {.inline.} =
     quit(1)
 
   info "lucem: installed sober successfully!"
-
-proc initializeRoblox*(input: Input, config: Config) {.inline.} =
-  info "lucem: an URL will now be opened in your browser."
-  info "Instructions:"
-  for instr in [
-    "Click on the gray button that says \"DOWNLOAD APK BUNDLE\"",
-    "Save the download in your Downloads folder. Do not rename it!",
-  ]:
-    echo "* " & instr
-
-  openDefaultBrowser(APKMIRROR_URL % [config.apk.version])
-
-  let confirmation =
-    readLineFromStdin("Have you downloaded the APK? If not, you can retry. [y/N]: ")
-  case confirmation.toLowerAscii()
-  of "y":
-    info "lucem: Sober will now be invoked. You need to pass on the APK to the GUI app that will now start."
-    flatpakRun(SOBER_APP_ID)
-
-    discard readLineFromStdin(
-      "Press Enter to continue once Sober tells you that the APK has been installed."
-    )
-
-    flatpakKill(SOBER_APP_ID)
-
-    info "lucem: Voila! Sober should now be properly configured and installed!"
-    info "lucem: To run Roblox, simply type:"
-    info "lucem: `lucem run`"
-    quit(0)
-  else:
-    info "lucem: Retrying."
-    initializeRoblox(input, config)
-    return
+  info "lucem: To run Roblox, simply type:"
+  info "lucem: `lucem run`"
+  quit(0)
